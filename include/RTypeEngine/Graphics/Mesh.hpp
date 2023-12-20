b5e77fbbf8dc1b73d2dbd9b6e295d2d2a75a6d43
Viltard Léo <leo.viltard@epitech.eu>
2023-12-20 14:23:51 +0100

    [++] WIP sprite management

---
+++ include/RTypeEngine/Graphics/Mesh.hpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2023 - Kleo
+ * Authors:
+ * - LEO VILTARD <leo.viltard@epitech.eu>
+ * NOTICE: All information contained herein is, and remains
+ * the property of Kleo © and its suppliers, if any.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Kleo ©.
+*/
+
+#pragma once
+
+#include "glm/glm.hpp"
+#include <iostream>
+#include <vector>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+namespace RTypeEngine {
+
+    /**
+     *  @brief Mesh component
+     *  @param VBO Vertex Buffer Object
+     *  @param VAO Vertex Array Object
+     *  @param EBO Element Buffer Object
+     *  @param vertices Vertices of the mesh
+     */
+    struct MeshComponent {
+        GLuint VBO;
+        GLuint VAO;
+    };
+
+    /**
+     * @brief Mesh static class
+     * @details This class is used to create and delete meshes
+     * @details It is also used to bind and unbind meshes
+     */
+    class Mesh {
+    public:
+        Mesh() = delete;
+        ~Mesh() = delete;
+        static MeshComponent createMesh(const float *vertices) noexcept;
+        static void deleteMesh(MeshComponent &mesh) noexcept;
+        static void bindMesh(MeshComponent &mesh) noexcept;
+        static void unbindMesh() noexcept;
+    };
+}
---
+++ include/RTypeEngine/Graphics/Sprite.hpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023 - Kleo
+ * Authors:
+ * - LEO VILTARD <leo.viltard@epitech.eu>
+ * NOTICE: All information contained herein is, and remains
+ * the property of Kleo © and its suppliers, if any.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Kleo ©.
+*/
+
+#pragma once
+
+#include "RTypeECS/Types.hpp"
+#include "RTypeECS/Coordinator.hpp"
+#include "Rect.hpp"
+#include "Texture.hpp"
+#include "Mesh.hpp"
+#include "Shader.hpp"
+
+
+namespace RTypeEngine {
+    /**
+     * \brief Sprite static class
+     * \details This class is used to manage sprites
+     */
+    class Sprite {
+    public:
+        Sprite() = delete;
+
+        ~Sprite() = delete;
+
+        static Entity create(Coordinator *coordinator) noexcept;
+
+        static void draw(Entity *entity, Coordinator *coordinator) noexcept;
+
+        static void
+        setTexture(Entity &entity, const std::string &path, Coordinator *coordinator) noexcept;
+
+        static void setTextureRect(Entity &entity, const RectI &rect, Coordinator *coordinator) noexcept;
+    };
+}
