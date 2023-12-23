b1b8a9e9398e8e4a153b00548659b19ea45f5f9a
QuentinBriand <quentin.briand@epitech.eu>
2023-12-23 13:53:00 +0100

    [~] Changed the way glm is fetched

--- external/FindGLM.cmake
+++ external/FindGLM.cmake
@@ -1,15 +1,15 @@
-find_library(GLM_LIB glm)
+# find_library(GLM_LIB glm)
 
-if(NOT GLM_LIB)
-    message(STATUS "Fetching GLM...")
+# if(NOT GLM_LIB)
+message(STATUS "Fetching GLM...")
 
-    FetchContent_Declare(
-        glm
-        GIT_REPOSITORY https://github.com/g-truc/glm.git
-        GIT_TAG master
-    )
+FetchContent_Declare(
+    glm
+    GIT_REPOSITORY https://github.com/g-truc/glm.git
+    GIT_TAG 0.9.8
+)
 
-    FetchContent_MakeAvailable(glm)
-else()
-    message(STATUS "GLM library found at ${GLM_LIB}")
-endif()
+FetchContent_MakeAvailable(glm)
+# else()
+#     message(STATUS "GLM library found at ${GLM_LIB}")
+# endif()
