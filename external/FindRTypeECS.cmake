70593c2fd9bed48695f4fb071b2714d2d05c0872
QuentinBriand <quentin.briand@epitech.eu>
2024-01-16 15:56:15 +0100

    [~] Changed to fetchcontent

--- external/FindRTypeECS.cmake
+++ external/FindRTypeECS.cmake
@@ -3,14 +3,13 @@ find_library(RTYPE-ECS_LIB rtype-ecs)
 if(NOT RTYPE-ECS_LIB)
     message(STATUS "Fetching RTypeECS...")
 
-    add_subdirectory(/home/qbriand/delivery/RType/RTypeECS RTypeECS)
-    # FetchContent_Declare(
-    #     rtype-ecs
-    #     GIT_REPOSITORY https://github.com/Vive-le-LEX/RTypeECS.git
-    #     GIT_TAG main
-    # )
+    FetchContent_Declare(
+        rtype-ecs
+        GIT_REPOSITORY https://github.com/Vive-le-LEX/RTypeECS.git
+        GIT_TAG main
+    )
 
-    # FetchContent_MakeAvailable(rtype-ecs)
+    FetchContent_MakeAvailable(rtype-ecs)
 else()
     message(STATUS "RTypeECS library found at ${RTYPE-ECS_LIB}")
 endif()
